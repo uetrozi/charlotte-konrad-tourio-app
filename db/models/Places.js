d82858b2ffafa804f97b27d06d6f1d570df12581
this.Konrad <konradlebt@gmail.com>
2024-01-10 17:55:36 +0100

    regex for image url

--- db/models/Places.js
+++ db/models/Places.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const placeSchema = new Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String, required: true, match: [/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&=]*)?/gi], },
   mapURL: { type: String, required: true },
   description: { type: String, required: true },
   comments: { type: [Schema.Types.ObjectId], ref: "Comments" },
